0d10a0c9e0d91e68064b7910f8afe57966a51084
Khandaker Sojol Ahmed <khandakervlogs@gmail.com>
2025-12-09 19:07:04 +0600

    Navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
-  return <div></div>;
+  const navLinks = (
+    <>
+      <li className="">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/services">Services</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-bold"
+          >
+            {navLinks}
+          </ul>
+        </div>
+
+        <NavLink
+          to="/"
+          className="text-2xl md:text-4xl font-bold text-gradient"
+        >
+          StyleDecor
+        </NavLink>
+      </div>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 font-semibold">{navLinks}</ul>
+      </div>
+      <div className="dropdown dropdown-end navbar-end">
+        <div
+          tabIndex={0}
+          role="button"
+          className="btn btn-ghost btn-circle avatar"
+        >
+          <div className="w-10 rounded-full">
+            <img src="https://i.pravatar.cc/100" alt="user" />
+          </div>
+        </div>
+        <ul
+          tabIndex="-1"
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-32 w-52 p-2 shadow font-semibold"
+        >
+          <li>
+            <Link to="">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="">Logout</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,36 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import ErrorPage from "../pages/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout></RootLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/services",
+        element: <Services></Services>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "*",
+        element: <ErrorPage></ErrorPage>,
+      },
+    ],
   },
 ]);
